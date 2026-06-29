4f19013bd04dce2f2d8464575b7899a6404912dd
Stefan Atanasov <80150302+devatanassov@users.noreply.github.com>
2026-06-29 19:04:23 +0300

    Fix: localize History and Advanced links in popup Simple view

--- popup/popup.js
+++ popup/popup.js
@@ -51,6 +51,8 @@ function applyPopupI18n() {
   setText('resumeBtn', 'advancedResume');
   setText('exportBtn', 'popupExportJson');
   setText('snapshotBtn', 'popupSnapshotJson');
+  setText('historyLink', 'popupHistory');
+  setText('advancedLink', 'popupAdvanced');
   setText('footerDisclaimer', 'footerDisclaimer');
   setLabel('#simpleView label[for=\"adapterId\"]', 'popupRetailer');
 }
